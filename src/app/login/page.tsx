bd5e9ffc57816d84cafe8ae4b09f5527172a1027
Ivan <contaivanrodrigues@gmail.com>
2025-09-04 15:14:45 -0300

    style: styling the login page with styles shadcn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,16 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -25,38 +35,59 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-sm rounded-2xl bg-white p-6 shadow-lg">
-        <h1 className="mb-6 text-center text-2xl font-bold">Login</h1>
+    <div className="flex h-screen items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
 
-        <form onSubmit={handleLogin}>
-          <Input
-            type="email"
-            placeholder="Email"
-            className="mb-4"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="password"
-            placeholder="Senha"
-            className="mb-4"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button type="submit" className="w-full mb-2">
+          <CardDescription>
+            Digite seu e-mail abaixo para acessar sua conta
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">Registrar</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleLogin}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Senha</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+
+        <CardFooter className="flex-col gap-2">
+          <Button onClick={handleLogin} type="submit" className="w-full mb-2">
             Entrar
           </Button>
-        </form>
-
-        <Button
-          variant="outline"
-          className="w-full"
-          onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
-        >
-          Entrar com Google
-        </Button>
-      </div>
+          <Button
+            type="button"
+            variant="outline"
+            className="w-full"
+            onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+          >
+            Entrar com Google
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
