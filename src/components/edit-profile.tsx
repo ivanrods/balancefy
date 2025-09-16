adcaf239d5995ec2fe29cb71a5cf9893794b880b
Ivan <contaivanrodrigues@gmail.com>
2025-09-16 10:55:55 -0300

    feat: add update profile in page

--- src/components/edit-profile.tsx
+++ src/components/edit-profile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -14,25 +14,52 @@ import {
 } from "@/components/ui/sheet";
 import { Avatar } from "@radix-ui/react-avatar";
 import { AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
 
-type Profile = {
-  name: string;
-  email: string;
-};
+const updateUserSchema = z.object({
+  name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
+  email: z.string().email("E-mail inválido"),
+});
+
+type updateFormData = z.infer<typeof updateUserSchema>;
 
 export function EditProfile() {
-  const [profile, setProfile] = useState<Profile | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<updateFormData>({ resolver: zodResolver(updateUserSchema) });
 
   useEffect(() => {
     async function fetchProfile() {
       const res = await fetch("/api/profile");
       if (res.ok) {
         const data = await res.json();
-        setProfile(data);
+        reset({
+          name: data.name,
+          email: data.email,
+        });
       }
     }
     fetchProfile();
-  }, []);
+  }, [reset]);
+
+  async function onSubmit(data: updateFormData) {
+    const res = await fetch("/api/profile", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...data }),
+    });
+
+    if (res.ok) {
+      alert("Perfil atualizado com sucesso!");
+    } else {
+      alert("Erro ao atualizar perfil.");
+    }
+  }
 
   return (
     <Sheet>
@@ -47,24 +74,39 @@ export function EditProfile() {
             terminar.
           </SheetDescription>
         </SheetHeader>
-        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+        <form
+          className="grid flex-1 auto-rows-min gap-6 px-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="flex justify-center">
             <Avatar className="mx-auto w-24 h-24">
               <AvatarImage src="/avatar.png" alt="User" className="h-24 w-24" />
-              <AvatarFallback>{profile?.name?.[0] ?? "?"}</AvatarFallback>
+              <AvatarFallback>{"?"}</AvatarFallback>
             </Avatar>
           </div>
           <div className="grid gap-3">
             <Label htmlFor="name">Nome</Label>
-            <Input id="name" placeholder="Seu nome" value={profile?.name} />
+            <Input type="text" placeholder="Nome" {...register("name")} />
+            {errors.name && (
+              <span className="text-red-500 text-sm">
+                {errors.name.message}
+              </span>
+            )}
           </div>
           <div className="grid gap-3">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="@email.com" value={profile?.email} />
+            <Input type="email" placeholder="E-mail" {...register("email")} />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message}
+              </span>
+            )}
           </div>
-        </div>
+        </form>
         <SheetFooter>
-          <Button type="submit">Salvar alterações</Button>
+          <Button type="submit" onClick={handleSubmit(onSubmit)}>
+            Salvar alterações
+          </Button>
           <SheetClose asChild>
             <Button variant="outline">Fechar</Button>
           </SheetClose>
