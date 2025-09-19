3d6f15beea7e511c1ef9daa22a46050348675554
Ivan <contaivanrodrigues@gmail.com>
2025-09-19 14:18:24 -0300

    feat: add get category by id and create category

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,10 +1,40 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
 
-export async function GET() {
-  const categories = await prisma.category.findMany({
-    select: { id: true, name: true },
-  });
+export async function GET(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
 
-  return NextResponse.json(categories);
+    if (!session?.user?.email) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email: session.user.email },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const categories = await prisma.category.findMany({
+      where: {
+        OR: [{ userId: user.id }, { userId: null }],
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    return NextResponse.json(categories);
+  } catch (err) {
+    console.error("Erro ao buscar categorias:", err);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
