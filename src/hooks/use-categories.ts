8f6a63d48524edbd82f23b24b1beb20c6f455cdc
Ivan <contaivanrodrigues@gmail.com>
2025-09-24 11:01:55 -0300

    feat: add hooks for categories

---
+++ src/hooks/use-categories.ts
@@ -0,0 +1,69 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { Categories } from "@/types/categories";
+
+export function useCategories() {
+  const queryClient = useQueryClient();
+
+  // GET
+  const { data, isLoading, error } = useQuery<Categories[]>({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const res = await fetch("/api/categories?type=summary");
+      if (!res.ok) throw new Error("Erro ao buscar transações");
+      return res.json();
+    },
+  });
+
+  // CREATE
+  const createCategories = useMutation({
+    mutationFn: async (categories: Omit<Categories, "id" | "createdAt">) => {
+      const res = await fetch("/api/categories", {
+        method: "POST",
+        body: JSON.stringify(categories),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (!res.ok) throw new Error("Erro ao criar categoria");
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+  });
+
+  //UPDATE
+  const updateCategories = useMutation({
+    mutationFn: async (categories: Categories) => {
+      const res = await fetch(`/api/transactions/${categories.id}`, {
+        method: "PUT",
+        body: JSON.stringify(categories),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (!res.ok) throw new Error("Erro ao atualizar categoria");
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+  });
+
+  // DELETE
+  const deleteCategories = useMutation({
+    mutationFn: async (id: string) => {
+      const res = await fetch(`/api/categories/${id}`, { method: "DELETE" });
+      if (!res.ok) throw new Error("Erro ao deletar categoria");
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+  });
+
+  return {
+    transactions: data,
+    isLoading,
+    error,
+    createCategories,
+    updateCategories,
+    deleteCategories,
+  };
+}
