0604c62a2c579991ae20dc5c5363ac2ac44e5a8c
Ivan <contaivanrodrigues@gmail.com>
2025-09-28 11:22:26 -0300

    feat: add link search dialog

--- src/components/dialogs/search-dialog.tsx
+++ src/components/dialogs/search-dialog.tsx
@@ -7,6 +7,7 @@ import {
   Info,
   Settings,
   User,
+  Wallet,
 } from "lucide-react";
 
 import {
@@ -19,6 +20,7 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
+import Link from "next/link";
 type SearchDialogProps = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -28,31 +30,35 @@ export function SearchDialog({ open, setOpen }: SearchDialogProps) {
   return (
     <div>
       <p className="text-muted-foreground text-sm">
-        <span className="hidden md:inline-block">Press</span>{" "}
+        <span className="hidden md:inline-block">click</span>{" "}
         <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
           <span className="text-xs">↵</span>
         </kbd>
       </p>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Type a command or search..." />
+        <CommandInput placeholder="Digite um comando ou pesquise..." />
         <CommandList>
           <CommandEmpty>Nenhum resultado encontrado.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
+          <CommandGroup heading="Sugestões">
+            <CommandItem>
+              <Wallet />
+              <Link href="wallet">Carteira</Link>
+            </CommandItem>
             <CommandItem>
               <ChartBarStacked />
-              <span>Categorias</span>
+              <Link href="categories">Categorias</Link>
             </CommandItem>
             <CommandItem>
               <ClipboardMinus />
-              <span>Relatórios</span>
+              <Link href="reports">Relatórios</Link>
             </CommandItem>
             <CommandItem>
               <Info />
-              <span>Ajuda</span>
+              <Link href="help">Ajuda</Link>
             </CommandItem>
           </CommandGroup>
           <CommandSeparator />
-          <CommandGroup heading="Settings">
+          <CommandGroup heading="Configurações">
             <CommandItem>
               <User />
               <span>Perfil</span>
