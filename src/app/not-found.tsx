75c823c19e8203aa6eb871343dd42a59d8c56a4f
Ivan <contaivanrodrigues@gmail.com>
2025-10-14 11:10:25 -0300

    refactor: fixed error responsiveness and hydration

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-4">
+    <div className="h-screen flex flex-col justify-center items-center gap-6 mx-2">
       <span className=" text-xl font-bold animate-bounce">404</span>
-      <h1 className="text-5xl text-primary font-bold">Página não encontrada</h1>
-      <p>Desculpe, não conseguimos encontrar a página que você procura.</p>
+      <h1 className="text-3xl md:text-5xl text-primary font-bold text-center">
+        Página não encontrada
+      </h1>
+      <p className="text-sm md:text-md text-center">
+        Desculpe, não conseguimos encontrar a página que você procura.
+      </p>
 
       <Button>
         <Link href="/">Voltar para o início</Link>
--- src/components/header/components/button-theme.tsx
+++ src/components/header/components/button-theme.tsx
@@ -1,11 +1,18 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import { Switch } from "@/components/ui/switch";
 import { useTheme } from "@/context/theme-context";
 import { Sun, Moon } from "lucide-react";
 
 export function ButtonTheme() {
   const { toggleTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <div className="relative flex items-center justify-center">
