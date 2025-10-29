9a9e79cce04169e526ee14d221a30952f3630538
Ivan <contaivanrodrigues@gmail.com>
2025-10-29 15:42:04 -0300

    feat: add notification alert component

--- src/components/header/components/notifications.tsx
+++ src/components/header/components/notifications.tsx
@@ -1,3 +1,4 @@
+import { Alert, AlertTitle } from "@/components/ui/alert";
 import {
   Popover,
   PopoverContent,
@@ -6,25 +7,26 @@ import {
 import { Bell, Megaphone } from "lucide-react";
 
 export function Notifications() {
+  const notifications = [{ id: 1, message: "Bem vindo(a) o Balancefy" }];
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Bell />
       </PopoverTrigger>
       <PopoverContent className="w-80 mx-4 my-2">
-        <div className="grid gap-4">
+        <div className="grid gap-2">
           <div className="space-y-2">
             <h4 className="leading-none font-medium">Notificações</h4>
             <p className="text-muted-foreground text-sm ">
               Suas notificações recentes
             </p>
           </div>
-          <div className="grid gap-2">
-            <div className="flex gap-2 p-3 rounded-md bg-secondary">
-              <Megaphone size={20} />{" "}
-              <span className="text-sm">Sem notificações</span>
-            </div>
-          </div>
+          {notifications.map((notification) => (
+            <Alert variant="default" key={notification.id}>
+              <Megaphone />
+              <AlertTitle>{notification.message}</AlertTitle>
+            </Alert>
+          ))}
         </div>
       </PopoverContent>
     </Popover>
