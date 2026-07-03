9d7f5a02933ad520cea0171f079e2e24c52d4579
Ivan <contaivanrodrigues@gmail.com>
2026-07-03 10:46:43 -0300

    test: fix setup to tests

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,6 @@ const createJestConfig = nextJest({ dir: "./" });
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
-  preset: "ts-jest",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,1 @@
-import { config } from "dotenv";
-config({ path: ".env.test" });
 import "@testing-library/jest-dom";
